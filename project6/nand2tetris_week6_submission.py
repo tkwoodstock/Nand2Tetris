f16cc9b427e966cb5a0df8fd2e86ad0d9f1fdecf
tkwoodstock <tyler.k.woodstock@gmail.com>
2024-06-19 12:38:22 +0100

    script to call assembly translator

---
+++ project6/nand2tetris_week6_submission.py
@@ -0,0 +1,15 @@
+from assembler1 import assembler
+
+
+test_base = "nand2tetris_project6/test_files/"
+test_tail = ".asm"
+
+files = ["Add", "Max", "Pong", "Rect"]
+#files = ["Pong"]
+
+outbase = "nand2tetris_project6/output/"
+out_tail = ".hack"
+
+for file in files:
+    assembler(test_base+file+test_tail, outbase+file+out_tail)
+
